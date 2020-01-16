df5701cff7030b77fe8b4144d959f7ce5438cebd
Javad Behrouzi pour <javad.behrouzi@ir.co.isc>
2020-01-16 15:33:06 +0330

    Auth added

--- app/auth.go
+++ app/auth.go
@@ -1,38 +1,60 @@
 package app
 
 import (
+	"PhoneBook/models"
 	"PhoneBook/utils"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var JwtAuthentication= func(next http.Handler) http.Handler{
-	return http.HandlerFunc(func(w http.ResponseWriter,r *http.Request) {
-		noAuth:=[]string{"/api/user/new","/api/user/login"}
-		requestPath:=r.URL.Path
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		noAuth := []string{"/api/user/new", "/api/user/login"}
+		requestPath := r.URL.Path
 
-		for _,value := range noAuth {
+		for _, value := range noAuth {
 			if value == requestPath {
-				next.ServeHTTP(w,r)
+				next.ServeHTTP(w, r)
 				return
 			}
 		}
-		response:=make(map[string] interface{})
-		tokenHeader:=r.Header.Get("Authorization")
-		if(tokenHeader==""){
-				response=utils.Message(false,"token not found")
-				w.WriteHeader(http.StatusForbidden)
-				w.Header().Add("Content-Type","application/json")
-				utils.Respond(w,response)
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Authorization")
+		if tokenHeader == "" {
+			response = utils.Message(false, "token not found")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			utils.Respond(w, response)
 		}
 		splitted := strings.Split(tokenHeader, " ")
-		if len(splitted)!=2{
-			response=utils.Message(false,"Invalid/Malformed auth token")
-			w.Header().Add("Content-Type","application/json")
+		if len(splitted) != 2 {
+			response = utils.Message(false, "Invalid/Malformed auth token")
+			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(http.StatusForbidden)
-			utils.Respond(w,response)
+			utils.Respond(w, response)
 		}
-		tokenPart:=splitted[1]
+		tokenPart := splitted[1]
 		tk := &models.Token{}
+		token, err := jwt.ParseWithClaims(tokenPart, tk, func(token *jwt.Token) (i interface{}, err error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+		if err != nil {
+			response = utils.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			utils.Respond(w, response)
+			return
+		}
+		if !token.Valid {
+			response = utils.Message(false, "token is not valid")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			utils.Respond(w, response)
+			return
+		}
+		fmt.Sprintf("User %", tk.UserId) //Useful for monitoring
 	})
 }
